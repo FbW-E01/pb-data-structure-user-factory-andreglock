1f1844ad21134bf35e6f8c6dd448ac14f3370cc2
André <andreglock@gmail.com>
2021-04-28 14:18:11 +0200

    done

--- start.js
+++ start.js
@@ -15,3 +15,38 @@ const userDataDump = [
   "D,Cheesedude51,1951,Users",
   "E,cron,,System",
 ];
+
+const makeUserObject = (userInput) => {
+  let userDataArray = [];
+  const userObject = [];
+  for (let i = 0; i < userInput.length; i++) {
+    userDataArray[i] = userInput[i].split(',');
+  }
+  for (let i = 0; i < userDataArray.length; i++) {
+    userObject.push( {
+      id: userDataArray[i][0],
+      fullName: userDataArray[i][1],
+      initials: userDataArray[i][1].slice(0,1),
+      birthYear: userDataArray[i][2],
+      departments: userDataArray[i][3].split('|'),
+    });
+  }
+
+  for (let i = 0; i < userObject.length; i++) {
+    if (userObject[i].id === '') {
+      userObject[i].id = 'Unknown';
+    } if (userObject[i].fullName === '') {
+      userObject[i].fullName = 'Unknown';
+    } if (userObject[i].initials === '') {
+      userObject[i].initials = 'Unknown';
+    } if (userObject[i].birthYear === '') {
+      userObject[i].birthYear = 'Unknown';
+    } if (userObject[i].departments === '') {
+      userObject[i].departments = 'Unknown';
+    }
+  }
+  
+  return userObject;
+};
+
+console.log(makeUserObject(userDataDump));
